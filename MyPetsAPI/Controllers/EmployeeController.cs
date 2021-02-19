0487b3db3e67418f682f46ad27b2e500644ff7b5
Nicolas <n.barile.57@gmail.com>
2021-02-19 13:17:11 +0100

    Correction , EmployeeDTO to EmployeeController

--- MyPetsAPI/Controllers/EmployeeController.cs
+++ MyPetsAPI/Controllers/EmployeeController.cs
@@ -9,31 +9,34 @@ using MyPetsCore.DTO;
 
 namespace MyPetsAPI.Controllers
 {
-    public class EmployeeDto : Controller
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : Controller
     {
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public ActionResult<List<EmployeeDto>> Get()
+        public ActionResult<List<EmployeeController>> Get()
         {
             try
             {
-                IServiceGet<EmployeeDto, EmployeeDao> getService = new IServiceGet<EmployeeDto, EmployeeDao>;
+                IServiceGet<EmployeeController, EmployeeDao> getService = new IServiceGet<EmployeeController, EmployeeDao>;
                 return getService.Get();
             }
-            catch (Exception)
+            catch (Exception e)
             {
+                Console.WriteLine(e);
                 return StatusCode(500);
             }
         }
 
         [HttpGet("{key}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public ActionResult<EmployeeDto> Get([FromRoute] uint key)
+        public ActionResult<EmployeeController> Get([FromRoute] uint key)
         {
             try
             {
                 if (key == 0) return StatusCode(400);
-                IServiceGet<EmployeeDto, EmployeeDao> getService = new IServiceGet<EmployeeDto, EmployeeDao>;
+                IServiceGet<EmployeeController, EmployeeDao> getService = new IServiceGet<EmployeeController, EmployeeDao>;
                 return getService.Get(key);
             }
             catch (Exception)
@@ -46,11 +49,11 @@ namespace MyPetsAPI.Controllers
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public ActionResult<bool> Post([FromBody] EmployeeDto employeeDto)
+        public ActionResult<bool> Post([FromBody] EmployeeController employeeDto)
         {
             try
             { 
-                IServiceCreate<EmployeeDto, EmployeeDao> createService = new IServiceCreate<EmployeeDto, EmployeeDao>;
+                IServiceCreate<EmployeeController, EmployeeDao> createService = new IServiceCreate<EmployeeController, EmployeeDao>;
                 return createService.Create(employeeDto);
             }
             catch (Exception)
@@ -61,11 +64,11 @@ namespace MyPetsAPI.Controllers
         
         // PUT api/values/5
         [HttpPut]
-        public ActionResult<bool> Put([FromBody] EmployeeDto employeeDto)
+        public ActionResult<bool> Put([FromBody] EmployeeController employeeDto)
         {
             try
             {
-                IServiceUpdate<EmployeeDto, EmployeeDao> updateService = new IServiceUpdate<EmployeeDto, EmployeeDao>;
+                IServiceUpdate<EmployeeController, EmployeeDao> updateService = new IServiceUpdate<EmployeeController, EmployeeDao>;
                 return updateService.Update(employeeDto);
             }
             catch (Exception)
@@ -80,7 +83,7 @@ namespace MyPetsAPI.Controllers
         {
             try
             { 
-                IServiceDelete<EmployeeDto, EmployeeDao> deleteService = new IServiceDelete<EmployeeDto, EmployeeDao>;
+                IServiceDelete<EmployeeController, EmployeeDao> deleteService = new IServiceDelete<EmployeeController, EmployeeDao>;
                 return deleteService.Delete(key);
             }
             catch (Exception)
