5a9d6fe38ba867b3b329dae4d7bb028b6cee2616
pierrewenzel17 <pierre.wenzel@outlook.com>
2021-04-26 22:03:25 +0200

    Suppression de SSL de Http dans le Site

--- MyPetsWeb/Startup.cs
+++ MyPetsWeb/Startup.cs
@@ -40,7 +40,7 @@ namespace MyPetsWeb
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
